9e7e783254482a9eb636d1832652ca2550532bf1
phat1202 <trg.tanphat@gmail.com>
2023-11-20 17:08:37 +0700

    update

--- Learning Management System/Controllers/TeacherController.cs
+++ Learning Management System/Controllers/TeacherController.cs
@@ -1,6 +1,7 @@
 ﻿using Learning_Management_System.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Learning_Management_System.Controllers
@@ -24,7 +25,36 @@ namespace Learning_Management_System.Controllers
                                                   .ToListAsync();
             return View(myCourses);
         }
+        public IActionResult CourseManagement(int courseId)
+        {
+            var chapters = _context.Chapters.Where(c => c.CourseId == courseId).ToList();
 
-
+            return View(chapters);
+        }
+        public IActionResult CreateChapter(int courseId)
+        {
+            var course = _context.Courses.ToList();
+            course.Insert(0, new Course()
+            {
+                CourseTitle = "Select Your Course",
+                CourseId = -1
+            });
+            ViewData["CourseId"] = new SelectList(course, "CourseId", "CourseTitle", -1);
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateChapter(Chapter chapter)
+        {
+            var newChapter = new Chapter
+            {
+                ChapterNumber = chapter.ChapterNumber,
+                ChapterTitle = chapter.ChapterTitle,
+                CourseId = chapter.CourseId,
+                TotalNumberOfLesson = chapter.TotalNumberOfLesson,
+            };
+            await _context.AddAsync(newChapter);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("CourseManagement");
+        }
     }
 }
---
+++ Learning Management System/Models/Ttttt.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Identity;
+using System.Diagnostics.Contracts;
+
+namespace Learning_Management_System.Models
+{
+    public class Ttttt
+    {
+        [PersonalData]
+        public virtual IKey? UserId { get; set; }
+    }
+}
